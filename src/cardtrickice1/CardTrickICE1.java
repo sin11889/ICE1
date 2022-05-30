a18986cb7ac72b414dc96129532b17234791733c
sin11889 <sin11889@sheridancollege.ca>
2022-05-30 14:29:04 -0400

    ICE 1

--- src/cardtrickice1/CardTrickICE1.java
+++ src/cardtrickice1/CardTrickICE1.java
@@ -5,6 +5,8 @@
  */
 package cardtrickice1;
 
+import java.util.Scanner;
+
 /** step1 : generate 7 random cards and store in array - how
  * step 2: take any card input from user suit,number
  * step 3: user card is in  the array 'card is found
@@ -30,9 +32,32 @@ public class CardTrickICE1 {
             magicHand[i]=c1;
             System.out.println(magicHand[i].getValue()+" of "+magicHand[i].getSuits());
         }
-        //step 2:take input 
+        Scanner input = new Scanner(System.in);
+        System.out.println("Please choose a value ( 1 to 12 ) : ");
+        int number = input.nextInt();
+        input.nextLine();
         
-        //step 3: match with array 
+        System.out.println("Please choose the suit :  ");
+        System.out.println("1. Diamonds");
+        System.out.println("2. Clubs");
+        System.out.println("3. Hearts");
+        System.out.println("4. Clubs");
+        String s = input.nextLine();
+        
+        CardTrickICE1 LuckyCard;
+        LuckyCard = new CardTrickICE1();
+       
+       
+        
+        //step 3: match with array
+        for (Card magicHand1 : magicHand) {
+            if (magicHand1.getValue() == number && magicHand1.getSuits().equalsIgnoreCase(s)) {
+                System.out.println("your card generated");
+                return;
+            }
+        }
+         System.out.println("Sorry, no match. ");
+        
     }
     
 }
